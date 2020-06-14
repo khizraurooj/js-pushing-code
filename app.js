580ecf3292fa4b99423ae398a9a58dd66caa2ac4
khizraurooj <65004555+khizraurooj@users.noreply.github.com>
2020-06-14 03:41:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,439 @@
+//Chapter no# 1
+//Task 1
+//alert("Hello world");
+//var a = 20;
+//var b = 85;
+//var c = a+b;
+//alert(c);
+
+ //Chapter no 2
+//Tasks 2
+//var a = 20;
+//var b = "35";
+//var c = a+b;
+
+//Task 3
+//var y = 9;
+//var x = "munza"
+//var z = y+x;
+//console.log(y+z);
+//alert(z);
+//Task 4
+//var k = "90";
+//var h = 60;
+//var n = k+h;
+//alert(n) 
+
+//Task 5
+//var a = 3;
+//var b = 10;
+//var c = 15;
+//var d = a + b + c;
+//alert(d);
+
+//Chapter no 3 
+//Task 1
+//var originalNum = 23;
+//var numToBeAdded = 7;
+//var newNum = originalNum + numToBeAdded;
+//alert(newNum)
+ 
+//Task 2
+//var num = 80;
+//var verb = 34;
+//var orgin = num - verb;
+//alert(orgin)
+
+//Chapter no 4
+//Task 1
+//var fog1 = 64;
+//var intial2 = 12
+//var intel = (fog1 * intial2)
+//alert(intel)
+
+//Task 2
+//var faceBook = 34;
+//var instaGram = 79;
+//var twitter = faceBook + instaGram;
+//alert(twitter)
+ 
+//Chapter no 5 
+//Task 1 
+//var a = 12;
+//var b = 2 + 2;
+//var c = ( a + b);
+//alert(c);
+
+//Task 2
+//var c = 2;
+//var d = 2;
+//var e = 6;
+//var f = (2 * 2) - 6;
+//alert(f);
+
+//Chapter no 6
+//Task 1
+//var num = 1;
+//var newNum = num++;
+//var logno = num * newNum;
+//alert(logno);
+
+//Chapter no 7
+//Task 1
+//var a = 4;
+//var b = 8;
+//var c = 9;
+//var e = a + (b * c)
+//alert(e);
+
+//Task 2
+//var totalCost = 1 + 3 * 4;
+
+
+//Task 3
+//var chatapp = 10;
+//var application = 12;
+//var ink = "20";
+//var f = application - ( chatapp + ink)
+//alert(f);
+
+//Chapter no 8
+//Task 1
+//var message = "Thanks for  supporting us!";
+// alert(message);
+
+// Task 2
+//var userName = "for "
+//var message = "Thanks, ";
+//var banger = "your output!";
+//alert(message + userName + banger);
+
+//Task 3
+//var message = "Hello, ";
+//var userName = " how's";
+//var banger = "about you";
+//var customMess = message + userName + banger;
+//alert(customMess);
+
+//Chapter no 9
+//Task 1
+//var newName = prompt("enter your name","your name is")
+//alert(newName);
+
+//Task 2
+//var question = "enter your age?";
+//var defaultAnswer = "19";
+//var spec = prompt(question, defaultAnswer);
+//alert(apec);
+
+//Chapter no 10
+//Task 1
+//var x = prompt("Where does the Pope live?");
+//if (x === "Vatican") {
+//alert("Correct!");
+//}
+
+//Task 2
+//var age = 32;
+//if(age ==54){
+// alert("your age is unless")
+//}
+
+//Chapter no 11
+//Task no 1
+//f (fullName === "Mark" + " " + "Myers") {
+//if (fullName === firstName + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+//if (fullName === "firstName + " " + lastName) {
+// if (totalCost === 81.50 + 135)
+//if (totalCost === materialsCost + 135) {
+//if (totalCost === materialsCost + laborCost) {
+//if (x + y === a - b) {
+
+//Task 2
+//var a = 10;
+//if (a!== 12){
+//document.write("correct");
+//}
+
+//Chapter no 12
+//Task no 1
+
+//var x = prompt("how we can help you?");
+//if (x === "Vatican") {
+//alert("Correct!");
+//}
+
+//Task 2
+//var correctAnswer = "Vatican";
+//if (x === correctAnswer) {
+//alert("Correct!");
+//} else {
+//score--;
+//userIQ = "problematic";
+//alert("Incorrect");
+//}
+//Task 3
+//var a = 50;
+//if("new iit!"){
+//alert( less)
+//}
+//else{
+//(unless)
+//};
+
+//Chapter no 13
+//Task 1
+//if (weight > 300 && time < 6) {
+//alert("Come to our tryout!");
+//}
+//else {
+//alert("Come to our cookout!");
+//}
+//Task 2
+//if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//alert("Come to our tryout!");
+//}
+//else {
+//alert("Come to our cookout!");
+//}
+    
+//Task 3
+//if (SAT > avg || GPA > 2.5 || sport === "football") {
+//alert("Welcome to Bubba State!");
+//}
+//else {
+//alert("Have you looked into appliance repair?");
+
+//Chapter no 14
+//Task 1
+//if ((x === y || a === b) && c === d) {
+//g = h;
+//}
+//else {
+//e = f;
+//}
+//Task 2
+//if (c === d) {
+//if (x === y) {
+// g = h;
+//}
+//else if (a === b) {
+// g = h;
+//}
+//else {
+// = f;
+/// }
+     
+//else {
+// e = f;
+// }
+
+//Chapter no 15
+//Task 1
+//var city0 = "Atlanta";
+//var city1 = "Baltimore";
+//var city2 = "Chicago";
+//var city3 = "Denver";
+//var city4 = "Los Angeles";
+//var city5 = "Seattle";
+//alert("Welcome to " + city3);
+ 
+//Task 2
+//var students = ["ahmed", "asad", "shahbaz"]
+
+//Chapter no 16
+//Task 1
+//var childs = [];
+//child[1] = "Ahmed";
+//child[2] = "Shahbaz";
+
+//Task 2
+//var students = ["ahmed", "asad", "shahbaz"]
+//students.pop(shabaz)
+
+//Task 3
+//var students = ["ahmed", "asad", "shahbaz"]
+//students.push(hania)
+
+//Chapter no 17
+//Task 1
+//var students = ["ahmed", "asad", "shahbaz"]
+//students.shift(ahmed)
+
+//students.unshift(hania)
+
+//Task 5var students = ["ahmed", "asad", "shahbaz"]
+//students.splice(0.1)
+
+//Chapter no 18
+
+//Task 1
+//if (cityToCheck === cleanestCities[0]) {
+//alert("It's one of the cleanest cities");
+//}
+//else if (cityToCheck === cleanestCities[1]) {
+//alert("It's one of the cleanest cities");
+//}
+//else if (cityToCheck === cleanestCities[2]) {
+//alert("It's one of the cleanest cities");
+//}
+//else if (cityToCheck === cleanestCities[3]) {
+//alert("It's one of the cleanest cities");
+//}
+//else if (cityToCheck === cleanestCities[4]) {
+//alert("It's one of the cleanest cities");
+//}
+
+//else {
+//alert("It's not on the list");
+//}
+
+//Chapter no 19
+//Task 1 
+//var cities = ["karachi","lahore","pishawar","islamabad"]; 
+//for(var i = 0; 1 < cities.length; i++){
+    //if (cities[i] === "karachi"){
+
+     //alert(cities[i] + "is the biggest city of light!")
+    //
+//}
+
+//Chapter no 20
+//Task 1
+
+//var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+//var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//var fullNames = [];
+//for (var i = 0; i < firstNames.length; i++) {
+//for (var j = 0; j < lastNames.length; j++) {
+//fullNames.push(firstNames[i] + lastNames[j]);
+
+//}
+//}
+
+//Chapter no 21
+//Task 1
+//var program = "STABLE";
+//program = program.toLowerCase();
+//console.log(program)
+
+//Task 2
+//var program = "STABLE";
+//program = program.toUpperCase();
+//console.log(program)
+
+//Chapter no 22
+//Task 1
+//var firstChar = cityToCheck.slice(0, 1);
+//console.log(firstChar)
+
+//Task 2
+//var someChars = cityToCheck.slice(2, 5);
+//var someChars = cityToCheck.slice(2);
+
+//Chapter no 23
+//Task 1
+//var skatch = "Khizra";
+//console.log(skatch.indexOf(r))
+
+//Task 2
+//for (var i = 0; i < text.length; i++) {
+//if (text.slice(i, i + 12) === "World War II") {
+//text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//}
+//}
+
+//Chapter no 24
+//Task 1
+//var char ="Ahmed Raza"; 
+//console.log(char.charAt(4))   
+
+//var char ="Ahmed Raza"; 
+//console.log(char.charAt(7))
+
+//Chapter no 25
+//Task 1
+//var para = "You are one of the intelligent person";
+//var para1 =  para.replace("person","girl")   
+//console.log(para1)
+
+//Task 2
+//var para = "You are one of the intelligent person,You are one of the intelligent person";
+//var para1 =  para.replace(/person/g,"girl")   
+//console.log(para1)
+
+//Chapter no 26
+//Task 1
+//var num = 5.6;
+//var goal = Math.round(num)
+//console.log(goal)
+
+//Task 2
+//var num = 1.8;
+//var goal = Math.round(num)
+//console.log(goal)
+
+//Task 3
+//var num = 9.6;
+//var goal = Math.ceil(num)
+//console.log(goal)
+
+//Task 4
+//var num = 8.4;
+//var goal = Math.floor(num)
+//console.log(goal)
+
+//Chapter no 27
+//Task 1
+//Math.random()*2; 
+//console.log(Mat.random)  
+
+//Task 2
+//var head = prompt("enter head");
+//var tail = prompt("enter tail");
+
+//var toss = Math.random() * 2;
+//var floor = Math.floor(toss)
+//if(floor === 0){
+    //alert("heads" + head + "win the toss")
+//}else{
+//alert("heads" + tail + "win the toss")
+
+//}
+//Chapter no 28
+//Task 1
+//Number("10")
+//console.log(Number)
+
+//Task 2
+//var doc = parseInt("20");
+//alert(doc)
+
+//Chapter no 29
+//Task 1
+//var num = 30;
+//console.log(num.toString())
+
+//Chapter no 30 
+
+//Task 1
+//var num = 20.5438769034;
+//var controling = num.toFixed(2)
+//alert(controling)
+
+//Task 2
+//var num = 50.6432129087;
+//var counting = num.toFixed(5)
+//alert(counting)
+
+//Fifth Assignment completed//
+
+
+
+
+
+
+
+
+
